92ac5345c847c95b82fd46f5ee5edeab2a96e975
Sina <sina.zahed026@gmail.com>
2024-01-27 12:20:46 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ docker exec -t shab-app php artisan key:generate
     echo "Waiting 14 second for database container be ready ..."
     sleep 14
 docker exec -t shab-app php artisan migrate --seed --force
-
+docker exec -t shab-app chown -R $USER:www-data storage
+docker exec -t shab-app chown -R $USER:www-data bootstrap/cache
