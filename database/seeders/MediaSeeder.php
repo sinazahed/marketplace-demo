c314b0a5092769b1f3417548b65d915dcc1734a9
sina <sina.zahed026@gmail.com>
2024-01-27 10:33:33 +0330

    Installation

---
+++ database/seeders/MediaSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Media;
+
+class MediaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Media::factory()->count(3)->create();
+    }
+}
